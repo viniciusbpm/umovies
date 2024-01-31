26fac06f5e156e69816987f450c71031c35f4ef4
Rafael <rafaelgoncalves.ssnfq@academico.ifsul.edu.br>
2024-01-30 23:22:17 -0300

    adding domain

---
+++ api/src/main/java/br/com/ubots/umovies/UmoviesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ubots.umovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmoviesApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/ubots/umovies/UmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ubots.umovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
